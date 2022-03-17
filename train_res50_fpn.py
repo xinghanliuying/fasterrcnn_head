e976e9a768859d939f4665a44a4b299ee9c37543
xinghanliuying <1498217034@qq.com>
2022-03-17 14:44:24 +0800

    no message

--- train_res50_fpn.py
+++ train_res50_fpn.py
@@ -170,7 +170,7 @@ def main(parser_data):
             save_files["scaler"] = scaler.state_dict()
         if epoch > (parser_data.epochs - 20):
             torch.save(save_files, "/kaggle/working/resNetFpn-model-{}.pth".format(epoch))
-        wandb.watch(model)
+        wandb.watch(model,log="all")
     # plot loss and lr curve
     if len(train_loss) != 0 and len(learning_rate) != 0:
         from plot_curve import plot_loss_and_lr
