275bc6be23a4c68cbd8a1348b4be9557daa4e615
xinghanliuying <1498217034@qq.com>
2022-03-18 17:14:10 +0800

    no message

--- train_utils/train_eval_utils.py
+++ train_utils/train_eval_utils.py
@@ -32,7 +32,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch,
         with torch.cuda.amp.autocast(enabled=scaler is not None):
             loss_dict = model(images, targets)
             losses = sum(loss for loss in loss_dict.values())
-        print(loss_dict)
+
         # reduce losses over all GPUs for logging purpose
         loss_dict_reduced = utils.reduce_dict(loss_dict)
         losses_reduced = sum(loss for loss in loss_dict_reduced.values())
@@ -61,7 +61,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch,
         metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
         now_lr = optimizer.param_groups[0]["lr"]
         metric_logger.update(lr=now_lr)
-
+    print(losses_reduced)
     return mloss, now_lr,
 
 
